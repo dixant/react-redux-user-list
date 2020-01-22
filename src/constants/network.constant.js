2861b9ca075d13649da21526aa372677da4171ba
vinee1127 <vinodkuril179@gmail.com>
2020-01-22 21:08:49 +0530

    heroku configured for API call

--- src/constants/network.constant.js
+++ src/constants/network.constant.js
@@ -1,4 +1,5 @@
 const NetworkConstant = {
-    baseURL: 'http://localhost:4000/'
+    //baseURL: 'http://localhost:4000/',
+    baseURL: 'https://react-redux-user-list.herokuapp.com/'
 }
 export default NetworkConstant;
