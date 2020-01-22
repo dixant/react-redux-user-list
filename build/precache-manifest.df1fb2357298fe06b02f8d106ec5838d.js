d17425a95834ccc1a1d22cf3093e684e1b0642a8
vinee1127 <vinodkuril179@gmail.com>
2020-01-22 21:19:43 +0530

    build push

---
+++ build/precache-manifest.df1fb2357298fe06b02f8d106ec5838d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0107dd09134f701702b4e5989e9c8383",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f2b627cd9b193bf06c6",
+    "url": "/static/css/2.8d392223.chunk.css"
+  },
+  {
+    "revision": "6918d4b94af2ecc897f8",
+    "url": "/static/css/main.9c705869.chunk.css"
+  },
+  {
+    "revision": "8f2b627cd9b193bf06c6",
+    "url": "/static/js/2.a3e0f061.chunk.js"
+  },
+  {
+    "revision": "6918d4b94af2ecc897f8",
+    "url": "/static/js/main.f7bedfca.chunk.js"
+  },
+  {
+    "revision": "4cb79f70a2f45246854a",
+    "url": "/static/js/runtime-main.1e028421.js"
+  },
+  {
+    "revision": "cffa9d9d88890cfcd0ae3d6dd7a6efa9",
+    "url": "/static/media/loadMore.cffa9d9d.gif"
+  }
+]);
